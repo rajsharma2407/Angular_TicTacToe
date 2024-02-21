4945e25dfcbcfb92bca40851bec233fa9a214e4a
Raj Sharma <sharmaraj22211@gmail.com>
2024-02-21 18:00:00 +0530

    first commit

---
+++ src/app/board/board.component.spec.ts
@@ -0,0 +1,44 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoardComponent } from './board.component';
+
+describe('BoardComponent', () => {
+  let component: BoardComponent;
+  let fixture: ComponentFixture<BoardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BoardComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BoardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should return the winner for each winning combination', ()=>{
+    const winningCombinations=[
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    
+  })
+});
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:rajsharma2407/Angular_TicTacToe.git
+git push -u origin main
---
+++ src/app/square/square.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  // templateUrl: './square.component.html',
+  template:`<button nbButton *ngIf="!value">{{ value }}</button>
+            <button nbButton hero status="success" *ngIf="value == 'X'" >{{ value }}</button>
+            <button nbButton hero status="info" *ngIf="value == 'O'" >{{ value }}</button>`,
+  styleUrls: ['./square.component.scss']
+})
+export class SquareComponent {
+  @Input() value!: 'X' | 'O';
+}
